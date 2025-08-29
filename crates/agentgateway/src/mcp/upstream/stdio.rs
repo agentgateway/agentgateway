9439742f9aa389113829d9f9cc5607019ab7c604
John Howard <john.howard@solo.io>
2025-08-29 11:49:53 -0700

    cleanup

--- crates/agentgateway/src/mcp/upstream/stdio.rs
+++ crates/agentgateway/src/mcp/upstream/stdio.rs
@@ -1,14 +1,31 @@
-use rmcp::transport::TokioChildProcess;
+use crate::mcp::upstream::UpstreamError;
+use rmcp::model::{ClientRequest, JsonRpcMessage, JsonRpcRequest};
+use rmcp::transport::{TokioChildProcess, Transport};
 use std::fmt;
 use std::fmt::{Debug, Formatter};
+use std::sync::{Arc, Mutex};
 
 pub struct Process {
-	inner: TokioChildProcess,
+	inner: Arc<Mutex<TokioChildProcess>>,
 }
 
 impl Process {
-	pub fn new(inner: TokioChildProcess) -> Self {
-		Self { inner }
+	pub async fn send_message(
+		&self,
+		req: JsonRpcRequest<ClientRequest>,
+	) -> Result<(), UpstreamError> {
+		let mut stream = self.inner.lock().unwrap();
+		stream.send(JsonRpcMessage::Request(req)).await?;
+		todo!()
+	}
+}
+
+impl Process {
+	pub fn new(mut proc: TokioChildProcess) -> Self {
+		let recv = proc.receive();
+		Self {
+			inner: Arc::new(Mutex::new(proc)),
+		}
 	}
 }
 
