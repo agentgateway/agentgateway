1a141b278412cf9a94a42b41fa0759768fe1abcb
John Howard <john.howard@solo.io>
2026-02-25 16:09:32 -0800

    route: do not split up routes by match
    This is an Istio-ism that is not relevant to AGW. its much more
    efficient to join them.

--- controller/pkg/agentgateway/translator/conversion.go
+++ controller/pkg/agentgateway/translator/conversion.go
@@ -41,9 +41,9 @@ import (
 
 // ConvertHTTPRouteToAgw converts a HTTPRouteRule to an agentgateway HTTPRoute
 func ConvertHTTPRouteToAgw(ctx RouteContext, r gwv1.HTTPRouteRule,
-	obj *gwv1.HTTPRoute, pos int, matchPos int,
+	obj *gwv1.HTTPRoute, pos int,
 ) (*api.Route, *reporter.RouteCondition) {
-	routeRuleKey := strconv.Itoa(pos) + "." + strconv.Itoa(matchPos)
+	routeRuleKey := strconv.Itoa(pos)
 	res := &api.Route{
 		// unique for route rule
 		Key:  utils.InternalRouteRuleKey(obj.Namespace, obj.Name, routeRuleKey),
