044a41ac8a87cb0aa43366082aab20442bb33fff
Markus Kobler <markus.kobler@solo.io>
2026-02-26 22:34:11 -0500

    Replace pemfile with rustls-pki-types

--- crates/agentgateway/tests/common/mock_ca_server.rs
+++ crates/agentgateway/tests/common/mock_ca_server.rs
@@ -1,15 +1,9 @@
 // Mock Istio Certificate Service for testing HBONE mTLS
-//
-// Since rcgen doesn't support CSR parsing (https://github.com/rustls/rcgen/issues/228),
-// we generate certificates with a static key and return the private key in the cert chain.
-// This is a test-only approach - real CAs never return private keys.
 
 use std::net::SocketAddr;
 use std::sync::Arc;
-use std::time::{Duration, SystemTime};
 
-use rand::Rng;
-use rcgen::{ExtendedKeyUsagePurpose, Issuer, KeyPair, KeyUsagePurpose, SanType, SerialNumber};
+use rcgen::{CertificateSigningRequestParams, Issuer, KeyPair};
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 
@@ -36,56 +30,20 @@ impl IstioCertificateService for MockCaService {
 		&self,
 		req: Request<IstioCertificateRequest>,
 	) -> Result<Response<IstioCertificateResponse>, Status> {
-		// Ignore the incoming CSR - we'll generate a new certificate
-		let _csr_pem = req.into_inner().csr;
+		// Parse the CSR from the request
+		let csr_pem = req.into_inner().csr;
+		let csr = CertificateSigningRequestParams::from_pem(&csr_pem)
+			.map_err(|e| Status::internal(format!("Failed to parse CSR: {}", e)))?;
 
-		// Generate random serial number (159 bits)
-		let serial_number = {
-			let mut data = [0u8; 20];
-			rand::rng().fill_bytes(&mut data);
-			data[0] &= 0x7f;
-			data
-		};
-
-		// Create CA issuer for signing workload certificates
-		let ca_kp = &*self.ca_key;
-		let ca_params = rcgen::CertificateParams::default();
-		let issuer = Issuer::new(ca_params, ca_kp);
-
-		// Generate a new key pair for the workload
-		let workload_kp = rcgen::KeyPair::generate_for(&rcgen::PKCS_ECDSA_P256_SHA256)
-			.map_err(|e| Status::internal(format!("Failed to generate key pair: {}", e)))?;
-
-		// Create new certificate parameters for the workload
-		let mut params = rcgen::CertificateParams::default();
-		params.not_before = SystemTime::now().into();
-		params.not_after = (SystemTime::now() + Duration::from_secs(365 * 24 * 60 * 60)).into();
-		params.serial_number = Some(SerialNumber::from_slice(&serial_number));
-
-		params.key_usages = vec![
-			KeyUsagePurpose::DigitalSignature,
-			KeyUsagePurpose::KeyEncipherment,
-		];
-		params.extended_key_usages = vec![
-			ExtendedKeyUsagePurpose::ServerAuth,
-			ExtendedKeyUsagePurpose::ClientAuth,
-		];
-
-		// Set SPIFFE ID as SAN
-		let spiffe_id = "spiffe://cluster.local/ns/default/sa/default";
-		params.subject_alt_names =
-			vec![SanType::URI(spiffe_id.try_into().map_err(|e| {
-				Status::internal(format!("Failed to create SAN: {}", e))
-			})?)];
-
-		// Sign the workload certificate with the CA using signed_by
-		let cert = params
-			.signed_by(&workload_kp, &issuer)
+		// Sign with CA issuer
+		let issuer = Issuer::from_ca_cert_pem(self.ca_cert_pem.as_str(), &*self.ca_key)
+			.map_err(|e| Status::internal(format!("Failed to load CA issuer: {}", e)))?;
+		let cert = csr
+			.signed_by(&issuer)
 			.map_err(|e| Status::internal(format!("Failed to sign certificate: {}", e)))?;
-		let cert_pem = cert.pem();
-		let key_pem = workload_kp.serialize_pem();
 
-		let cert_chain = vec![cert_pem, key_pem, self.ca_cert_pem.to_string()];
+		let cert_pem = cert.pem();
+		let cert_chain = vec![cert_pem, self.ca_cert_pem.to_string()];
 
 		Ok(Response::new(IstioCertificateResponse { cert_chain }))
 	}
