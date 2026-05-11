0a30b376995a742ed1a47fd6c341bb1db904dbe9
Alan Bentley <alanbentleypgh@gmail.com>
2026-05-11 13:17:16 -0400

    include commit for ui-e2e workflow changes, added data-testid to a few buttons to support readonly tests, implemented LLM Config and MCP Config readony tests Signed-off-by: Alan Bentley <alanbentleypgh@gmail.com>

--- ui/tests/xdsMode.spec.ts
+++ ui/tests/xdsMode.spec.ts
@@ -1,21 +1,22 @@
 import { expect, test } from '@playwright/test';
 
-test('xDS banner should be visible app-wide across all pages', async ({ page }) => { 
-    // TODO: update with merics/log routes when these are made available again
-    const routes = [
-        '/ui#/dashboard',
-        '/ui#/llm-configuration',
-        '/ui#/llm-playground',
-        '/ui#/llm-configuration/editor',
-        '/ui#/mcp-configuration',
-        '/ui#/mcp-playground',
-        '/ui#/mcp-configuration/editor',
-        '/ui#/traffic-configuration',
-        '/ui#/traffic-configuration/editor',
-        '/ui#/cel-playground',
-    ];
+// TODO: update with merics/log routes when these are made available again
+const AGENTGATEWAY_ROUTES = [
+    '/ui#/dashboard',
+    '/ui#/llm-configuration',
+    '/ui#/llm-playground',
+    '/ui#/llm-configuration/editor',
+    '/ui#/mcp-configuration',
+    '/ui#/mcp-playground',
+    '/ui#/mcp-configuration/editor',
+    '/ui#/traffic-configuration',
+    '/ui#/traffic-configuration/editor',
+    '/ui#/cel-playground',
+];
 
-    for (const route of routes) { 
+test('xDS banner should be visible app-wide across all pages', async ({ page }) => { 
+
+    for (const route of AGENTGATEWAY_ROUTES) { 
         // navigate to route
         await page.goto(route);
     
@@ -26,4 +27,54 @@ test('xDS banner should be visible app-wide across all pages', async ({ page }) 
         await expect(xdsBanner).toContainText('Configuration is managed by xDS');
         await expect(xdsBanner).toContainText('This agentgateway is receiving its configuration from https://localhost:18000. Edits are disabled.');
     }
+});
+
+test('LLM Configuration should be in read-only mode', async ({ page }) => { 
+    // navigate to LLM Configuration page
+    await page.goto('/ui#/llm-configuration');
+
+    // verify add buttons are disabled with tooltip
+    const hierarchyTreeAddButton = page.getByTestId('hierarchy-tree-no-resources-add-button');
+    await expect(hierarchyTreeAddButton).toBeVisible();
+    await expect(hierarchyTreeAddButton).toBeDisabled();
+
+    const llmConfigAddButton = page.getByTestId('llm-config-no-resources-add-button');
+    await expect(hierarchyTreeAddButton).toBeVisible();
+    await expect(hierarchyTreeAddButton).toBeDisabled();
+
+    await expect(llmConfigAddButton).toBeVisible();
+    await expect(llmConfigAddButton).toBeDisabled();
+});
+
+test('LLM Editor should be in read-only mode', async ({ page }) => { 
+    // TODO
+});
+
+test('MCP Configuration should be in read-only mode', async ({ page }) => { 
+    // navigate to MCP Configuration page
+    await page.goto('/ui#/mcp-configuration');
+
+    // verify add buttons are disabled with tooltip
+    const hierarchyTreeAddButton = page.getByTestId('hierarchy-tree-no-resources-add-button');
+    await expect(hierarchyTreeAddButton).toBeVisible();
+    await expect(hierarchyTreeAddButton).toBeDisabled();
+
+    const llmConfigAddButton = page.getByTestId('mcp-config-no-resources-add-button');
+    await expect(hierarchyTreeAddButton).toBeVisible();
+    await expect(hierarchyTreeAddButton).toBeDisabled();
+
+    await expect(llmConfigAddButton).toBeVisible();
+    await expect(llmConfigAddButton).toBeDisabled();
+});
+
+test('MCP Editor should be in read-only mode', async ({ page }) => { 
+    // TODO
+});
+
+test('Traffic Configuration should be in read-only mode', async ({ page }) => { 
+    // TODO
+});
+
+test('Traffic Editor should be in read-only mode', async ({ page }) => { 
+    // TODO
 });
