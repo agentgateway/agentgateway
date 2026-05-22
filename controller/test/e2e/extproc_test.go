3f896d15e0495faf1888defc025538d1064b7b7c
John Howard <john.howard@solo.io>
2026-05-22 12:26:35 -0700

    Add ext proc tests back
    Signed-off-by: John Howard <john.howard@solo.io>

--- controller/test/e2e/extproc_test.go
+++ controller/test/e2e/extproc_test.go
@@ -24,6 +24,9 @@ func TestExtProc(tt *testing.T) {
 	t.Run("HTTPRouteTargetRef", func(t base.Test) {
 		testExtProcWithHTTPRouteTargetRef(t)
 	})
+	t.Run("ProcessingOptionsRequestHeaderSkip", func(t base.Test) {
+		testExtProcProcessingOptionsRequestHeaderSkip(t)
+	})
 }
 
 // testExtProcWithGatewayTargetRef tests ExtProc with targetRef to Gateway using AgentgatewayPolicy
@@ -102,6 +105,55 @@ func testExtProcWithHTTPRouteTargetRef(t base.Test) {
 		{
 			name: "route without ExtProc should not have header modified",
 			url:  "www.example.com",
+			opts: []curl.Option{
+				curl.WithHeader("instructions", getInstructionsJson(instructions{
+					AddHeaders: map[string]string{"extproctest": "true"},
+				})),
+			},
+			resp: &testmatchers.HttpResponse{
+				StatusCode: http.StatusOK,
+				Body: gomega.WithTransform(transforms.WithEchoHeaders(),
+					gomega.Not(gomega.HaveKeyWithValue("Extproctest", "true")),
+				),
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t base.Test) {
+			t.Send(tc.url, tc.resp, tc.opts...)
+		})
+	}
+}
+
+// testExtProcProcessingOptionsRequestHeaderSkip validates that requestHeaderMode=Skip
+// suppresses request-header processing while default ext_proc settings still mutate headers.
+func testExtProcProcessingOptionsRequestHeaderSkip(t base.Test) {
+	t.Apply(manifest("extproc", "processing-options-request-header-skip.yaml"))
+
+	testCases := []struct {
+		name string
+		url  string
+		opts []curl.Option
+		resp *testmatchers.HttpResponse
+	}{
+		{
+			name: "default extproc route mutates request headers",
+			url:  "default-extproc.example.com",
+			opts: []curl.Option{
+				curl.WithHeader("instructions", getInstructionsJson(instructions{
+					AddHeaders: map[string]string{"extproctest": "true"},
+				})),
+			},
+			resp: &testmatchers.HttpResponse{
+				StatusCode: http.StatusOK,
+				Body: gomega.WithTransform(transforms.WithEchoHeaders(),
+					gomega.HaveKeyWithValue("Extproctest", "true"),
+				),
+			},
+		},
+		{
+			name: "requestHeaderMode skip route does not mutate request headers",
+			url:  "skip-extproc.example.com",
 			opts: []curl.Option{
 				curl.WithHeader("instructions", getInstructionsJson(instructions{
 					AddHeaders: map[string]string{"extproctest": "true"},
