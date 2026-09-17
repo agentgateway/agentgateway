261e4dd27a41e237a37ef63ff2e86a763bcbd7c8
Nathan Fudenberg <nathan.fudenberg@solo.io>
2026-09-17 10:39:39 -0400

    crates: respond with tool errors on tool calls that hit guardrail and rate limit errors
    Signed-off-by: Nathan Fudenberg <nathan.fudenberg@solo.io>

--- crates/agentgateway/src/proxy/mod.rs
+++ crates/agentgateway/src/proxy/mod.rs
@@ -504,7 +504,7 @@ impl ProxyError {
 			ProxyError::MCP(mcp::Error::Unavailable(_, _)) => StatusCode::SERVICE_UNAVAILABLE,
 			// Note: we do not return a 401/403 here, as the obscure that it was rejected due to auth
 			ProxyError::MCP(mcp::Error::Authorization(_, _, _)) => StatusCode::BAD_REQUEST,
-			ProxyError::MCP(mcp::Error::McpGuardrails(_, _)) => StatusCode::OK,
+			ProxyError::MCP(mcp::Error::McpGuardrails { .. }) => StatusCode::OK,
 			ProxyError::MCP(mcp::Error::RateLimited { .. }) => StatusCode::OK,
 		};
 		let grpc_status = is_grpc_request.then(|| proxy_error_to_grpc_status(&self, code));
